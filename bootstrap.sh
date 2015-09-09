c19eae2263616aaeecfc593de7e12f057e086810
Amir <szitenberg@gmail.com>
2015-09-09 13:03:46 +0100

    install phylobayes

--- bootstrap.sh
+++ bootstrap.sh
@@ -60,10 +60,12 @@ cd ~/tools
 wget -c http://trimal.cgenomics.org/_media/trimal.v1.2rev59.tar.gz
 wget -c http://www.evolution.rdg.ac.uk/BayesTraitsV2.0Files/Linux64/BayesTraitsV2.tar.gz
 wget -c http://www.bork.embl.de/pal2nal/distribution/pal2nal.v14.tar.gz
+wget -c http://megasun.bch.umontreal.ca/People/lartillot/www/phylobayes4.1b.tar.gz
 # extract
 tar -zxvf trimal.v1.2rev59.tar.gz
 tar -zxvf BayesTraitsV2.tar.gz
 tar -zxvf pal2nal.v14.tar.gz
+tar -zxvf phylobayes4.1b.tar.gz
 rm *.tar.gz
 # install trimal
 cd trimAl/source
@@ -76,11 +78,15 @@ chmod a+x BayesTraitsV2
 cd ../pal2nal.v14
 chmod a+x pal2nal.pl
 
-# move files to vagrant home
-chown vagrant:vagrant /vagrant/reprophylo/raxmlHPC-PTHREADS-SSE3
+# install phylobayes
+cd ../phylobayes4.1b/data/
+chmod a+x pb bpcomp tracecomp
+
+# install raxmlHPC-PTHREADS-SSE3
 chmod a+x /vagrant/reprophylo/raxmlHPC-PTHREADS-SSE3
-mv /vagrant/reprophylo/raxmlHPC-PTHREADS-SSE3 ~/bin/.
-cp ~/bin/raxmlHPC-PTHREADS-SSE3 ~/tools/.
+cp /vagrant/reprophylo/raxmlHPC-PTHREADS-SSE3 ~/tools/.
+
+# move files to vagrant home
 mv ~/bin /home/vagrant
 mv ~/tools /home/vagrant
 cp /vagrant/reprophylo /home/vagrant
@@ -97,7 +103,10 @@ ln -s /home/vagrant/tools/trimAl/source/trimal /home/vagrant/bin/trimal
 ln -s /home/vagrant/tools/trimAl/source/readal /home/vagrant/bin/readal
 ln -s /home/vagrant/tools/BayesTraitsV2/BayesTraitsV2 /home/vagrant/bin/BayesTraitsV2
 ln -s /home/vagrant/tools/pal2nal.v14/pal2nal.pl /home/vagrant/bin/pal2nal.pl
-ln -s /home/vagrant/tools/pal2nal.v14/raxmlHPC-PTHREADS-SSE3 /home/vagrant/bin/raxmlHPC-PTHREADS-SSE3
+ln -s /home/vagrant/tools/raxmlHPC-PTHREADS-SSE3 /home/vagrant/bin/raxmlHPC-PTHREADS-SSE3
+ln -s /home/vagrant/tools/phylobayes4.1b/data/pb /home/vagrant/bin/pb
+ln -s /home/vagrant/tools/phylobayes4.1b/data/tracecomp /home/vagrant/bin/tracecomp
+ln -s /home/vagrant/tools/phylobayes4.1b/data/bpcomp /home/vagrant/bin/bpcomp
 
 # fix ownership
 chown -R vagrant:vagrant /home/vagrant
